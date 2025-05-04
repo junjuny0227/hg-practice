77d9b38ea09a2745853b35b2ee33ea68236b289a
junjuny0227 <junjuny0227@gmail.com>
2025-05-04 18:42:24 +0900

    tanstackQuery provider

---
+++ src/app/provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren } from "react";
+
+const Provider = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;

514707fe9b85ea665ef0d29182a525d70e7cb1aa
junjuny0227 <junjuny0227@gmail.com>
2025-05-06 14:49:25 +0900

    select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,27 @@
 import {
-  Button,
-  Pagination,
-  PaginationContent,
-  PaginationLink,
-  PaginationItem,
-  PaginationPrevious,
-  PaginationNext,
-  PaginationEllipsis,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components";
 
 const Main = () => {
   return (
     <div>
-      <Button>button</Button>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink isActive href="#">
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="성적 선택" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="A">A</SelectItem>
+          <SelectItem value="B">B</SelectItem>
+          <SelectItem value="C">C</SelectItem>
+          <SelectItem value="D">D</SelectItem>
+          <SelectItem value="E">E</SelectItem>
+          <SelectItem value="None">없음</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
